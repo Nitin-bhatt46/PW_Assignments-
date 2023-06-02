e29a5ef81270febc0d42cbf52d1be259f1d9f235
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2023-06-02 20:49:46 +0530

    Add files via upload

---
+++ 29_jan_assignment.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+ASSIGASSIGNMENT 1 (29-JAN)
+
+Q1 Who developed Python Programming language ?
+
+Ans:- Created by Guido van Rossum
+
+
+
+# In[ ]:
+
+
+
+
+
+
+2. Which type of Programmin does Python supports ?
+
+
+Ans :- Python is a high-level, general-purpose programming language,It is a multi-paradigm programming language. Object-oriented programming and structured programming are fully supported, and many of their features support functional programming and aspect-oriented programming.
+# In[ ]:
+
+
+
+
+
+
+
+3. Is Python case sensitive when dealing with identifiers ?
+
+Ans :- It is a case_ sensitive programming language.
+# In[ ]:
+
+
+
+
+
+
+
+4. What is the correct extension of the Python file?
+
+Ans :- .py is the extension for python file
+# In[ ]:
+
+
+
+
+
+
+
+5. Is Python code compiled or Interpreted ?
+
+
+Ans :- It is Interpreted. In this line are processed by line by the interpretor.
+# In[ ]:
+
+
+
+
+
+
+
+
+6. Name  a few blocks of code uesd to define in Python Language ?
+
+ANs:- The following are blocks: a module, a function body, and a class definition. Each command typed interactively is a block. A script file (a file given as standard input to the interpreter or specified as a command line argument to the interpreter) is a code block.
+
+
+# In[11]:
+
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+7. State a charcter used to give single- line comments in python ? 
+
+Ans :-  # 
+
+
+# In[ ]:
+
+
+
+
+
+
+
+8. Mention functions which can help us to find the version of python that are we are curently working on ?
+
+ans :-  from platform import python_version
+    
+         python_version()
+
+or 
+
+
+!python --version
+
+
+
+or 
+
+import sys
+
+sys.version
+
+# In[12]:
+
+
+
+
+
+9 Python supports the creation of ananymous functions at runtime, using a construct called
+
+Ans :- lambda
+
+# In[ ]:
+
+
+
+
+
+
+
+
+10. What does pip stands for python ?
+
+Ans:- Preferred Installer Program
+# In[ ]:
+
+
+
+
+
+
+
+11. Mention a few built-in function in python ?
+
+Ans :- ascii() , compile(), float() , min(), max(), dict()
+
+
+# In[ ]:
+
+
+
+
+
+
+
+12. What is the maximum possible length of an identifier in Python ? 
+
+Ans :- 79 characters
+
+# In[ ]:
+
+
+
+
+
+
+13. What are the benefits of using Python ?
+
+Ans :-  Simple to Use and Understand,Free and Open-Source,Interpreted Language,Supportive community etc
+
+# In[ ]:
+
+
+
+
+
+
+14. How is memeory managed in python ?
+ans :- Memory management in Python involves a private heap containing all Python objects and data structures, it is dynamic in nature.
+
+# In[ ]:
+
+
+
+
+
+
+15. How to install python on WindoWs and set path variables ?
+Ans :- follow the steps :- 
+
+1 :- Open Any internet Browser.
+2 :- visit www.python.org
+3 :- Click on the download tab and then select he windows option.
+4 :- set path variables :-
+
+Right click on My Computer ->Properties ->Advanced System setting ->Environment Variable ->New
+
+In Variable name write path and in Variable value copy path up to C://Python(i.e., path where Python is installed). Click Ok ->Ok.
+# In[ ]:
+
+
+
+
+
+
+
+
+16. Is indentation required in python ? 
+
+Ans:- It is the most important thing in python without it python is not a language.
